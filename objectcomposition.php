699462d24debe9f85c2960498f4bd431625a2256
remakeaccess22 <alayacyacjude@gmail.com>
2024-07-16 14:12:14 +0800

    Object Composition

---
+++ objectcomposition.php
@@ -0,0 +1,50 @@
+<?php
+
+class Subscription {
+
+    protected $gateway;
+
+    public function __construct(Gateway $gateway) {
+
+        $this->gateway = $gateway; // Dependency Injection
+
+    }
+
+    public function create() {
+
+        var_dump('subscribing');
+        
+    }
+
+    public function cancel() {
+
+        $this->gateway->findCustomer();
+
+    }
+
+    public function invoice() {
+
+    }
+
+}
+
+interface Gateway {
+    
+        public function findCustomer();
+        public function findSubscriptionByCustomer();
+}
+
+class StripeGateway implements Gateway {
+
+
+    public function findCustomer() {
+
+
+    }
+
+    public function findSubscriptionByCustomer() {
+
+    }
+
+
+}
